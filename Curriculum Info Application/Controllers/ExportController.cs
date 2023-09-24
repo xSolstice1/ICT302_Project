d6f15c0bb1f83e79cbdf850f07cac61ed9826f99
Tay-CK <145850777+Tay-CK@users.noreply.github.com>
2023-09-24 22:15:37 +0800

    1. Temporary remove login feature 2. Added Import and Export Controller 3. Added join key selection UI

---
+++ Curriculum Info Application/Controllers/ExportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Curriculum_Info_Application.Controllers
+{
+    public class ExportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Curriculum Info Application/Controllers/ImportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Curriculum_Info_Application.Controllers
+{
+    public class ImportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
