d97ed9d631f759b5a4a96300e30a0c277e8697d5
Tay-CK <145850777+Tay-CK@users.noreply.github.com>
2023-09-26 23:48:27 +0800

    Added Header column list selection for join key

--- Curriculum Info Application/Controllers/ImportController.cs
+++ Curriculum Info Application/Controllers/ImportController.cs
@@ -11,12 +11,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Curriculum_Info_Application.Controllers
 {
     public class ImportController : Controller
     {
         private readonly string connectionString = "Server=tcp:ict302database.database.windows.net,1433;Initial Catalog=Testing;User ID=testadmin;Password=@Testing;Encrypt=True;"; // Replace with your Azure SQL Database connection string
+        private List<List<String>> columnHeadersList = new List<List<String>>();
+
+        public IActionResult Index()
+        {
+            ViewBag.ColumnsList1 = new SelectList(new List<SelectListItem>(), "Value", "Text");
+            ViewBag.ColumnsList2 = new SelectList(new List<SelectListItem>(), "Value", "Text");
+            return View();
+        }
 
         [HttpPost]
         public async Task<IActionResult> ProcessImport(List<IFormFile> files)
@@ -70,7 +79,22 @@ namespace Curriculum_Info_Application.Controllers
                     await SaveDataToAzureDatabaseAsync(records);
                 }
 
+                // Check files quantity
+                if (files.Count >= 2)
+                {
+                    ViewBag.ColumnsList1 = new SelectList(columnHeadersList[0]);
+                    ViewBag.ColumnsList2 = new SelectList(columnHeadersList[1]);
+                }
+                else
+                {
+                    ViewBag.ColumnsList1 = new SelectList(new List<string>());
+                    ViewBag.ColumnsList2 = new SelectList(new List<string>());
+                }
+
+                //return View("Index");
+
                 return Ok("Data imported and saved successfully.");
+                //return View();
             }
             catch (Exception ex)
             {
@@ -89,6 +113,7 @@ namespace Curriculum_Info_Application.Controllers
                 {
                     var record = new List<string>();
                     csv.ReadHeader(); //read csv header
+                    columnHeadersList.Add(csv.HeaderRecord.ToList());
                     for (int i = 0; i < csv.HeaderRecord.Length; i++)
                     {
                         record.Add(csv.GetField(i));
@@ -108,7 +133,14 @@ namespace Curriculum_Info_Application.Controllers
             {
                 var worksheet = package.Workbook.Worksheets[0]; // Assuming you're working with the first worksheet
                 var rowCount = worksheet.Dimension.Rows;
+                var headerRecord = new List<string>();
+                for (int col = 1; col <= worksheet.Dimension.Columns; col++)
+                {
+                    headerRecord.Add(worksheet.Cells[1, col].Text); // Assuming header is in the first row
+                }
 
+                // Add the header to columnHeadersList
+                columnHeadersList.Add(headerRecord);
                 for (int row = 1; row <= rowCount; row++)
                 {
                     var record = new List<string>();
@@ -209,6 +241,54 @@ namespace Curriculum_Info_Application.Controllers
                 }
             }
         }
+
+        [HttpPost]
+        public IActionResult JoinTables(string selectedColumn1, string selectedColumn2)
+        {
+            try
+            {
+                // Assuming table1 and table2 are the tables you want to join
+                // Replace these with your actual tables and column names
+                string table1 = "Table1";
+                string table2 = "Table2";
+
+                // Join logic based on the selected columns
+                string joinQuery = $@"
+                    SELECT *
+                    FROM {table1}
+                    INNER JOIN {table2} ON {table1}.{selectedColumn1} = {table2}.{selectedColumn2}";
+
+                List<string> joinResults = new List<string>();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(joinQuery, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Modify this part based on your actual result processing
+                                string resultRow = $"{reader[selectedColumn1]} - {reader[selectedColumn2]}";
+                                joinResults.Add(resultRow);
+                            }
+                        }
+                    }
+                }
+
+                // For demonstration purposes, let's pass the join results to the view
+                ViewBag.JoinResults = joinResults;
+
+                return View("JoinResults"); // Create a view to display the join results
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
     }
 }
 
