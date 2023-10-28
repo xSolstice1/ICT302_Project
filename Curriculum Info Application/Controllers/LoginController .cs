d4fa5a835fa202c5b4afd7e2d34633c80bb64b94
Tay-CK <145850777+Tay-CK@users.noreply.github.com>
2023-10-28 15:25:22 +0800

    Modify login UI

--- Curriculum Info Application/Controllers/LoginController .cs
+++ Curriculum Info Application/Controllers/LoginController .cs
@@ -1,19 +1,52 @@
-﻿using Curriculum_Info_Application.Models;
+﻿using System.Data.OleDb;
+using Curriculum_Info_Application.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Curriculum_Info_Application.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _connection;
+        private OleDbConnection _conn;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = _configuration.GetConnectionString("DefaultConnection");
+            _conn = new OleDbConnection(_connection);
+        }
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
         public IActionResult Login(LoginModel model)
         {
-            // Implement login logic here
-            return RedirectToAction("Import");
+            if (LoginModel.checkCredential(_connection, model))
+            {
+                return RedirectToAction("Import");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Invalid username or password.";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Signup(LoginModel model)
+        {
+            if (LoginModel.insertNewUser(_connection, model))
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Registration failed. Please try again.";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
