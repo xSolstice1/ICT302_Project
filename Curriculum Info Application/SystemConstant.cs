8284aad81f8549aaeb87339583d12db2cc576090
Tay-CK <145850777+Tay-CK@users.noreply.github.com>
2023-10-29 20:52:53 +0800

    Added system constant class

---
+++ Curriculum Info Application/SystemConstant.cs
@@ -0,0 +1,14 @@
+﻿namespace Curriculum_Info_Application
+{
+    public class SystemConstant
+    {
+        // file path
+        public static readonly string ROOT_PATH = "../Curriculum Info Application/data/";
+        public static readonly string LOGIN_FILEPATH = ROOT_PATH + "login.json";
+        public static readonly string USER_FILEPATH = ROOT_PATH + "user.json";
+        public static readonly string TRANSACTION_FILEPATH = ROOT_PATH + "transaction.json";
+        public static readonly string DATA1_FILEPATH = ROOT_PATH + "Data1.xml";
+        public static readonly string DATA2_FILEPATH = ROOT_PATH + "Data2.xml";
+        public static readonly string JOINEDDATA_FILEPATH = ROOT_PATH + "JoinedData.xml";
+    }
+}
